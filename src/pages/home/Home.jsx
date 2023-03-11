ba3a74493c871e23b3d72808477fd0c049b8b6d8
italeenamk <italeenamk@gmail.com>
2023-03-11 21:50:14 +0500

    create slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Top_Nav from "../layout/Top_Nav";
+import Slider from "../layout/Slider";
 
 
 const Home = () => {
   return(
       <>
           <Top_Nav/>
+          <Slider/>
       </>
   )
 }
