2023edcb57177d5881cf402aec6d5aa2b86dbac7
italeenamk <italeenamk@gmail.com>
2023-03-24 21:13:48 +0500

    react-router

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,24 @@
-
-import Home from "./pages/home/Home";
+import {render} from "react-dom";
+import {Router, Routes, BrowserRouter as Route, BrowserRouter} from "react-router-dom";
 import About_main from "./pages/home/About_main";
 import Shop from "./pages/home/Shop";
 import Blog_main from "./pages/home/Blog_main";
 import Contact_us from "./pages/home/Contact_us";
+import Home from "./pages/home/Home";
 
 const Main = () => {
   return(
       <>
+          <BrowserRouter>
+              <Routes>
+                      <Route path="/" element={<Home/>}/>
+                      <Route path="/about" element={<About_main/>}/>
+                      {/*<Route path="contact" element={<Contact />} />*/}
+                      {/*<Route path="*" element={<NoPage />} />*/}
+              </Routes>
+          </BrowserRouter>
           {/*<Home/>*/}
-          <About_main/>
+          {/*<About_main/>*/}
           {/*<Shop/>*/}
           {/*<Blog_main/>*/}
           {/*<Contact_us/>*/}
--- src/pages/home/About_main.jsx
+++ src/pages/home/About_main.jsx
@@ -1,14 +1,11 @@
 import Top_Nav from "../layout/Top_Nav";
-import Slider from "../layout/Slider";
 import About_slider from "../layout/About_slider";
 import About from "../layout/About";
 import Footer from "../layout/Footer";
 import Hijab from "../layout/Hijab";
-import View_card from "../layout/View_card";
 import About_card from "../layout/About_card";
 import Quality from "../layout/Quality";
 import Story from "../layout/Story";
-import Services from "../layout/Services";
 
 
 const About_main = () => {
