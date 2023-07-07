55bd03dd7fba9832fa283b6362b6993ee1fc5ee2
italeenamk <italeenamk@gmail.com>
2023-07-07 20:25:03 +0500

    adding routing

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,13 @@
 import {render} from "react-dom";
 import {Router, Routes, BrowserRouter as Route, BrowserRouter} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 import Contact_us from "./pages/home/Contact_us";
 
 const Main = () => {
+
+    const currentUser = true;
+    const user = false;
   return(
       <>
             {/*<Contact_us/>*/}
