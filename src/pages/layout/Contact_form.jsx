3a84983acd799c8809ef08438e13fc9b66cda4c3
italeenamk <italeenamk@gmail.com>
2023-07-07 20:13:59 +0500

    adding map

--- src/pages/layout/Contact_form.jsx
+++ src/pages/layout/Contact_form.jsx
@@ -24,28 +24,32 @@ const Contact_form = () => {
                           <div className="contact-map-content">
                               <div className="row">
                                   <div className="col-md-8 col-sm-12">
-                                      <div className="contact-me-location-content">
-                                          <div className="card">
-                                              <div className="row">
-                                                  <div className="col-md-8 col-sm-12">
-                                                      <div className="contact-page-card-left-content">
-                                                          <h4>lastminute.com London Eye</h4>
-                                                          <p>Riverside Building, County Hall, London SE1 7PB, United Kingdom</p>
-                                                          <h5>4.5 <a href="">151,949 reviews</a></h5>
-                                                          <span>View larger map</span>
-                                                      </div>
-                                                  </div>
-                                                  <div className="col-md-4 col-sm-12 text-center">
-                                                      <div className="contact-page-right-content">
-                                                          <i className="fa-solid fa-signs-post fa-1x"></i>
-                                                          <div className="contact-page-card-left-content">
-                                                              <span>Direction</span>
-                                                          </div>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      </div>
+                                      {/*<div className="contact-me-location-content">*/}
+                                      {/*    <div className="card">*/}
+                                      {/*        <div className="row">*/}
+                                      {/*            <div className="col-md-8 col-sm-12">*/}
+                                      {/*                <div className="contact-page-card-left-content">*/}
+                                      {/*                    <h4>lastminute.com London Eye</h4>*/}
+                                      {/*                    <p>Riverside Building, County Hall, London SE1 7PB, United Kingdom</p>*/}
+                                      {/*                    <h5>4.5 <a href="">151,949 reviews</a></h5>*/}
+                                      {/*                    <span>View larger map</span>*/}
+                                      {/*                </div>*/}
+                                      {/*            </div>*/}
+                                      {/*            <div className="col-md-4 col-sm-12 text-center">*/}
+                                      {/*                <div className="contact-page-right-content">*/}
+                                      {/*                    <i className="fa-solid fa-signs-post fa-1x"></i>*/}
+                                      {/*                    <div className="contact-page-card-left-content">*/}
+                                      {/*                        <span>Direction</span>*/}
+                                      {/*                    </div>*/}
+                                      {/*                </div>*/}
+                                      {/*            </div>*/}
+                                      {/*        </div>*/}
+                                      {/*    </div>*/}
+                                      {/*</div>*/}
+                                      <iframe
+                                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d106428.10877525054!2d72.72249275850982!3d33.530547068860535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df9b8a0fbc4dbb%3A0x7a23bdc39fa31808!2sJangal%2C%20Attock%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1688742769968!5m2!1sen!2s"
+                                          width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy"
+                                          referrerPolicy="no-referrer-when-downgrade"></iframe>
                                   </div>
                                   <div className="col-md-4 col-sm-12"></div>
                               </div>
